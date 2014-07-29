5b58e9899cc714fc4c03c31979f6424160976800
Sergey Yeremenko <syeremenko@live.com>
2014-07-29 01:34:50 -0400

    Day 4
    Rock, Paper, Scissors

--- day4/summary/js/workers.js
+++ day4/summary/js/workers.js
@@ -1,11 +1,23 @@
 (function () {
 
-  document.onmousemove = function(){
+var shootButton = document.getElementById("says-shoot");
+var resultsPlaceholder = document.getElementById("results");
 
-  var doThis = function () {
-    alert('yes');
-  };
-  doThis();
+var selections = [
+  "Rock",
+  "Paper",
+  "Scissors"
+];
+
+var saysShoot = function () {
+  var randomHand = selections[Math.floor(Math.random() * selections.length)];
+  resultsPlaceholder.classList.add('highlight');
+  resultsPlaceholder.innerHTML = randomHand;
+};
+
+shootButton.onclick = function (evt) {
+  evt.preventDefault();
+  saysShoot();
 };
 
 }());
